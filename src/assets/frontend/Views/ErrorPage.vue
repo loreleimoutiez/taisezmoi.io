aca7f0916e7cd88ff93e80029c4ce87602ef92f3
Lorelei <loreleimoutiez@gmail.com>
2023-12-31 12:20:22 +0100

    Feat: vue router & pages

---
+++ src/assets/frontend/Views/ErrorPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import LayoutComp from '@/assets/frontend/Components/LayoutComp.vue'
+</script>
+
+<template>
+    <LayoutComp>
+        <div class="flex flex-col items-center mt-48">
+            <h1 class="text-9xl text-warning">404</h1>
+            <router-link :to="'/'" class="text-lg text-white font-bold underline mt-5">Retourner à la page d'accueil</router-link>
+            <img src="@/assets/images/dino.png" class="h-20 md:h-48 mt-28 animate-bounce">
+        </div>
+    </LayoutComp>
+</template>
