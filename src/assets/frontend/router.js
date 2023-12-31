e9347d796c50f2e62133f71a7ba2dc13ae2d590d
Lorelei <loreleimoutiez@gmail.com>
2023-12-31 12:20:22 +0100

    Feat: vue router & pages

---
+++ src/assets/frontend/router.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import HomePage from '@/assets/frontend/Views/HomePage.vue'
+import AboutPage from '@/assets/frontend/Views/AboutPage.vue'
+import ProjectsPage from '@/assets/frontend/Views/ProjectsPage.vue'
+import BlogPage from '@/assets/frontend/Views/BlogPage.vue'
+import ErrorPage from '@/assets/frontend/Views/ErrorPage.vue'
+
+const routes = [
+    {
+      path: '/',
+      name: 'Accueil',
+      component: HomePage,
+    },
+    {
+      path: '/about',
+      name: 'À propos',
+      component: AboutPage,
+    },
+    {
+      path: '/projects',
+      name: 'Projets',
+      component: ProjectsPage,
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: BlogPage,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: '404',
+      component: ErrorPage,
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory('/'),
+    routes
+})
+
+export default router
