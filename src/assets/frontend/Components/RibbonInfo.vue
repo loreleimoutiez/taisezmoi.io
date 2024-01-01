d8396495fa3451159dc3edaed526d661123a7824
Lorelei <loreleimoutiez@gmail.com>
2024-01-01 13:11:09 +0100

    Feat: about page

--- src/assets/frontend/Components/RibbonInfo.vue
+++ src/assets/frontend/Components/RibbonInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-end overflow-hidden relative">
+  <div class="flex items-end overflow-hidden relative z-40">
     <div class="fixed top-6 right-10 h-20 w-20">
       <div class="transform rotate-45 bg-info text-center text-white font-semibold py-1 right-[-35px] top-[32px] w-[170px]">
         WIP
