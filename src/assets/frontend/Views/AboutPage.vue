98d6b5f1665c6ef2074439cf78acf298eb4d4a32
Lorelei <loreleimoutiez@gmail.com>
2024-01-01 13:11:09 +0100

    Feat: about page

--- src/assets/frontend/Views/AboutPage.vue
+++ src/assets/frontend/Views/AboutPage.vue
@@ -4,6 +4,46 @@ import LayoutComp from '@/assets/frontend/Components/LayoutComp.vue'
 
 <template>
     <LayoutComp>
-        <h1>À propos</h1>
+        <div class="bg-gradient-to-b from-base-100 to-black">
+            <img class="w-full object-none md:object-cover md:h-64 opacity-50" src="@/assets/images/ordi.jpg" alt="photographie d'un écran d'ordinateur avec du code un peu flou">
+        </div>
+        <div class="flex flex-col items-center ">
+            <div class="-mt-20 z-10">
+                <img class="w-32 rounded-full p-6 border-opacity-5 bg-base-100" src="@/assets/images/coin.png" alt="photo d'un canard en plastique jaune">
+            </div>
+            <p class="text-xs -mt-1 opacity-90">(Photo non contractuelle)</p>
+            <p class="text-5xl text-white my-4">Loreleï</p>
+            <div class="flex flex-col items-start mx-3 my-4">
+                <h1 class="text-base mt-1">Learning about #DataEngineer ing (and other things)</h1>
+                <p class="text-base mt-1 text-white font-bold">+ dev en alternance</p>
+                <p class="text-base mt-3">
+                    <span class="md:hidden block">
+                        <ul>
+                            <li>
+                                <a href="https://www.codecademy.com/" target="_blank" rel="noopener noreferrer" class="text-yellow-100">[ 'Codecademy',</a>
+                            </li>
+                            <li>
+                                <a href="https://openclassrooms.com/fr/" target="_blank" rel="noopener noreferrer" class="text-indigo-500 ml-2">'OpenClassrooms',</a>
+                            </li>
+                            <li>
+                                <a href="https://www.wildcodeschool.com/fr-fr/" target="_blank" rel="noopener noreferrer" class="text-pink-500 ml-2">'Wild Code School',</a>
+                            </li>
+                            <li>
+                                <a href="https://www.akawaka.fr/" target="_blank" rel="noopener noreferrer" class="text-amber-500 ml-2">'Akawaka' ]</a>
+                            </li>
+                        </ul>
+                    </span>
+                    <span class="hidden md:inline-block">
+                        <a href="https://www.codecademy.com/" target="_blank" rel="noopener noreferrer" class="text-yellow-100">Codecademy</a> / 
+                        <a href="https://openclassrooms.com/fr/" target="_blank" rel="noopener noreferrer" class="text-indigo-500">OpenClassrooms</a> / 
+                        <a href="https://www.wildcodeschool.com/fr-fr/" target="_blank" rel="noopener noreferrer" class="text-pink-500">Wild Code School</a> / 
+                        <a href="https://www.akawaka.fr/" target="_blank" rel="noopener noreferrer" class="text-amber-500">Akawaka</a>
+                    </span>
+                </p>
+            </div>
+            <div class="flex flex-col items-start mx-3 my-20">
+                <p>Je crois qu'il me faut plus qu'une fin de journée après le travail pour écrire cette page.</p>
+            </div>
+        </div>
     </LayoutComp>
 </template>
