06035a0813118ec5108e6276a32f92e72bcd5de4
Lorelei <loreleimoutiez@gmail.com>
2024-03-18 15:39:24 +0100

    hopefully it'll fix things

--- src/backend/routes/article.js
+++ src/backend/routes/article.js
@@ -2,7 +2,19 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
+const multer = require('../middlewares/multer');
 const articleCtrl = require('../controllers/article');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, new Date().toISOString() + file.originalname);
+    }
+  });
+
+const upload = multer({ storage: storage });
 
 router.get('/', articleCtrl.getAllArticles);
 router.post('/', auth, upload.single('image'), articleCtrl.createArticle);
