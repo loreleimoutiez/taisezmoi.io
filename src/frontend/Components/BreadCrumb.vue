9c6e36bf85f61ac2a1de0e0eef4b805f814c7ab5
Lorelei <loreleimoutiez@gmail.com>
2025-01-26 11:54:13 +0100

    add dist

--- src/frontend/Components/BreadCrumb.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <nav class="text-sm text-gray-600 mb-6 flex items-center space-x-2">
-      <router-link to="/blog" class="hover:text-gray-900 font-medium">
-        Taiseznews
-      </router-link>
-      <span> &gt; </span>
-      <span class="font-medium text-gray-900">Article</span>
-    </nav>
-</template>
